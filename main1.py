830f5c92f6f1d182c63d7ee97b8ce921f1bda19b
Mityashina Daria <mityashina.06@list.ru>
2025-09-17 16:25:12 +0300

    В main1.py добавлена функция вычитания, в  script2.py добавлена строка вывода

--- main1.py
+++ main1.py
@@ -3,3 +3,10 @@ def sum(a, b):
 
 
 print(sum(5, 3))
+
+
+def sub(a, b):
+    return abs(a - b)
+
+
+print(sub(5, 3))
---
+++ script2.py
@@ -0,0 +1,6 @@
+print("цвет: ", end=" ")
+s = "красный"
+if "синий" in s:
+    print("YES")
+else:
+    print("NO")
